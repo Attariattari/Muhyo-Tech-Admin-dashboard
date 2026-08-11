ddff3a8675a3becda08413ac017fee8b461cbab5
Pir Ghulam Muhyo Din <104420695+Attariattari@users.noreply.github.com>
2026-08-11 15:40:07 +0500

    Remove middleware.js in favor of proxy.js for Next.js 16

--- src/middleware.js
+++
@@ -1,9 +0,0 @@
-import { proxy } from "./proxy";
-
-export async function middleware(request) {
-  return proxy(request);
-}
-
-export const config = {
-  matcher: ["/admin/:path*", "/api/admin/:path*"],
-};
